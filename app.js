cf10104f70b68a56ad793eec9efa85114ada2523
Richard Willis <willis.rh@gmail.com>
2013-04-29 11:44:17 +0100

    Added director for client-side routing

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// routes
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
