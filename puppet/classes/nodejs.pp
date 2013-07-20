887101bd9fbe9b553bfa7c2ab79b7d5d25623ce1
Richard Willis <willis.rh@gmail.com>
2013-07-20 14:53:03 +0100

    Added comments to nodejs manifest file

--- puppet/classes/nodejs.pp
+++ puppet/classes/nodejs.pp
@@ -1,20 +1,26 @@
 class nodejs {
 
+  # Ensure the python-software-properties package is available
+  # to allow us to add apt repositories.
   package { 'python-software-properties':
     ensure  => present,
     require => Exec['apt-update'],
   }
 
+  # Add the chri-lead/node.js ppa repository so we can install 
+  # the latest version of nodejs.
   exec { 'apt-repository-nodejs':
     command => '/usr/bin/add-apt-repository ppa:chris-lea/node.js',
     require => Package['python-software-properties'],
   }
 
+  # Update the list of packages.
   exec { 'apt-update-nodejs':
     command => '/usr/bin/apt-get update',
     require => Exec['apt-repository-nodejs']
   }
 
+  # Ensure the nodejs package is installed.
   package { 'nodejs':
     ensure  => present,
     require => Exec['apt-update-nodejs']
